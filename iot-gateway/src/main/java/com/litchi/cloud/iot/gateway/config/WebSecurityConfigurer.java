10018510bffdbc72d70ba3f331f53a4e8dc841b0
LitchiInn <941895102@qq.com>
2019-08-11 17:18:38 +0800

    增加跨域配置

---
+++ iot-gateway/src/main/java/com/litchi/cloud/iot/gateway/config/WebSecurityConfigurer.java
@@ -0,0 +1,24 @@
+package com.litchi.cloud.iot.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/** 
+ * 类说明
+ * @author: tievd(wjhf) 
+ * @date: 2019年8月8日 上午11:30:11
+ * @vesion: 0.0.1
+ */ 
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+		http.headers().frameOptions().disable();
+        http.csrf().disable();
+    }
+
+}
